5400af73f32d5d2b144e72c9793d863af4c4aef5
zzh <zzh@mac>
2019-06-24 23:41:27 +0800

    20190624

--- index.ts
+++ index.ts
@@ -1,21 +1,6 @@
 import { Framework } from '@vechain/connex-framework'
 import { DriverNodeJS } from '@vechain/connex.driver-nodejs'
-import { cry, Transaction } from 'thor-devkit'
-
-// function buffer2hexstr(b: Buffer): string {
-//     return '0x' + b.toString('hex');
-// }
-
-// function genPrivateKey(): [string, string] {
-//     const sk_buffer = cry.secp256k1.generatePrivateKey();
-//     const addr = buffer2hexstr(cry.publicKeyToAddress(cry.secp256k1.derivePublicKey(sk_buffer)));
-//     const sk = buffer2hexstr(sk_buffer);
-//     return [sk, addr];
-// }
-
-// let x: string;
-// let y: string;
-// [x, y] = genPrivateKey();
+import { cry } from 'thor-devkit'
 
 const sk = '0x29a9c5eabe185f68abeb41f4d68e04a5004c146eaa3fd8a76aa3a87b33b6f1a7';
 const acc1 = '0x8580C3BFF10f2886B7CF183a8Eb51e76d75B42c4';
@@ -31,54 +16,107 @@ const acc2 = '0x91436f1E5008B2E6093E114A25842F060012685d';
     const signinigService = connex.vendor.sign('tx');
     signinigService.signer(acc1);
 
+    let txids: string[] = [];
+    let ret = undefined;
+
+    // 
+    ret = await sendNotRevertedTX(signinigService);
+    txids.push(ret.txid);
+    console.log('Broadcast TX with ID = ' + ret.txid);
+    console.log('...');
+
+    //
+    ret = await sendRevertedTX(signinigService, connex);
+    txids.push(ret.txid);
+    console.log('Broadcast TX with ID = ' + ret.txid);
+    console.log('...');
+
+    ret = await sendTestTX(signinigService, txids[0]);
+    txids.push(ret.txid);
+    console.log('Broadcast TX with ID = ' + ret.txid);
+    console.log('...');
+
+    ret = await sendTestTX(signinigService, txids[1]);
+    txids.push(ret.txid);
+    console.log('Broadcast TX with ID = ' + ret.txid);
+    console.log('...');
+
+    checkTXs(connex, txids);
+})();
+
+function sendNotRevertedTX(signinigService: Connex.Vendor.SigningService<'tx'>) {
     signinigService.gas(21000);
-    let ret = await signinigService.request([
+    return signinigService.request([
         {
             to: acc2,
             value: '1000000000000000000',
             data: '0x'
         }
     ]);
-    const txid1 = ret.txid;
-    console.log(txid1);
-    // const receipt1 = await connex.thor.transaction(txid1).getReceipt();
-    // console.log('Reverted: ' + receipt1.reverted);
+}
 
+function sendRevertedTX(signinigService: Connex.Vendor.SigningService<'tx'>, connex: Connex) {
     const energy_token_addr = '0x0000000000000000000000000000456E65726779';
     const transferABI = { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_amount", "type": "uint256" }], "name": "transfer", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" };
     const transferMethod = connex.thor.account(energy_token_addr).method(transferABI);
     const energyClause = transferMethod.asClause(acc2, '1000000000000000000000');
 
     signinigService.gas(30000)
-    ret = await signinigService.request([
+    return signinigService.request([
         {
             ...energyClause
         }
     ]);
-    const txid2 = ret.txid;
-    console.log(txid2);
-    // const receipt2 = await connex.thor.transaction(txid2).getReceipt();
-    // console.log('Reverted: ' + receipt1.reverted);    
+}
 
-    signinigService.gas(21000).dependsOn(txid1);
-    ret = await signinigService.request([
+function sendTestTX(signinigService: Connex.Vendor.SigningService<'tx'>, txid: string) {
+    signinigService.gas(21000).dependsOn(txid);
+    return signinigService.request([
         {
             to: acc2,
             value: '1000000000000000000',
             data: '0x'
         }
     ]);
-    const txid3 = ret.txid;
-    console.log(txid3);
+}
 
-    signinigService.gas(21000).dependsOn(txid2);
-    ret = await signinigService.request([
-        {
-            to: acc2,
-            value: '1000000000000000000',
-            data: '0x'
+async function checkTXs(connex: Connex, txids: string[]) {
+    const MAX_ITER = 3;
+    const checker = [false, false, false, false];
+    const ticker = connex.thor.ticker();
+    for (let i = 0; i < MAX_ITER; i++) {
+        await ticker.next();
+
+        console.log('--------------------------')
+        console.log('Block Number = ' + connex.thor.status.head.number);
+        console.log('--------------------------')
+        for (let j in txids) {
+            if (checker[j]) {
+                continue;
+            }
+
+            console.log('...');
+            console.log('Checking TX with ID = ' + txids[j]);
+            console.log('...');
+
+            const ret = await connex.thor.transaction(txids[j]).getReceipt();
+            if (ret === null) {
+                console.log('TX not found!');
+            } else {
+                console.log('TX found!');
+                checker[j] = true;
+            }
         }
-    ]);
-    const txid4 = ret.txid;
-    console.log(txid4);
-})();
+        console.log('...');
+    }
+}
+
+function buffer2hexstr(b: Buffer): string {
+    return '0x' + b.toString('hex');
+}
+
+function genPrivateKey(): [string, string] {
+    const sk = cry.secp256k1.generatePrivateKey();
+    const addr = cry.publicKeyToAddress(cry.secp256k1.derivePublicKey(sk));
+    return [buffer2hexstr(sk), buffer2hexstr(addr)];
+}
